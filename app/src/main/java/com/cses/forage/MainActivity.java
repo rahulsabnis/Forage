16bca63c1c4e4c8bbfcda14f7df876559d0198f0
Megan Wang <mmwangemail@gmail.com>
2017-07-29 17:05:16 -0700

    More firebase stuff

--- app/src/main/java/com/cses/forage/MainActivity.java
+++ app/src/main/java/com/cses/forage/MainActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public Firebase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        database = new Firebase();
+        database.writeString();
+        //database.readString();
     }
 }
