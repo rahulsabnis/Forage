6d9138b3cb3e9c564db310e28bf05272567caaab
Joel Sequeira <joelseq96@gmail.com>
2017-07-30 11:11:59 -0700

    Animate splash page

--- app/src/main/java/com/cses/forage/SplashActivity.java
+++ app/src/main/java/com/cses/forage/SplashActivity.java
@@ -3,19 +3,30 @@ package com.cses.forage;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    Animation fadeIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        // Animate fade in for splash image
+        ImageView splashImage = (ImageView) findViewById(R.id.splash_img);
+        fadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+        fadeIn.setDuration(2000);
+        splashImage.startAnimation(fadeIn);
+
         Thread myThread = new Thread() {
             @Override
             public void run() {
                 try {
-                    sleep(3000);
+                    sleep(1000);
                     Intent transitionToMain = new Intent(getApplicationContext(), MainActivity.class);
                     startActivity(transitionToMain);
                     finish();
