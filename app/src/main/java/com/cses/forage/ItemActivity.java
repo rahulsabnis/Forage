9447c1ce52a60f394bc8f8313acd3a08f3252844
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-07-30 04:00:58 -0700

    App can be demoed

--- app/src/main/java/com/cses/forage/ItemActivity.java
+++ app/src/main/java/com/cses/forage/ItemActivity.java
@@ -1,11 +1,21 @@
 package com.cses.forage;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+
+import com.travijuu.numberpicker.library.Enums.ActionEnum;
+import com.travijuu.numberpicker.library.Interface.ValueChangedListener;
+import com.travijuu.numberpicker.library.NumberPicker;
 
 public class ItemActivity extends AppCompatActivity {
 
@@ -13,16 +23,46 @@ public class ItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item);
+
+        Bundle extras = getIntent().getExtras();
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(extras.getString("itemName"));
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        AppCompatButton submit = (AppCompatButton) findViewById(R.id.submitButton);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(ItemActivity.this)
+                        .setTitle("Success!")
+                        .setMessage("Your order was successfully placed!")
+                        .setPositiveButton("OK", null)
+                        .create();
+                dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        Intent acceptOrder = new Intent(ItemActivity.this, MainActivity.class);
+                        startActivity(acceptOrder);
+                    }
+                });
+                dialog.show();
             }
         });
+
+        final Double initCost = extras.getDouble("cost");
+        final TextView view = (TextView) findViewById(R.id.total);
+        view.setText(String.format("Total Cost: $%.2f", initCost));
+
+        NumberPicker picker = (NumberPicker) findViewById(R.id.picker);
+        picker.setValueChangedListener(new ValueChangedListener() {
+            @Override
+            public void valueChanged(int value, ActionEnum action) {
+                Double updatedCost = initCost * value;
+                view.setText(String.format("Total Cost: $%.2f", updatedCost));
+            }
+        });
+        AppBarLayout layout = (AppBarLayout) findViewById(R.id.app_bar);
+        layout.setBackgroundResource(extras.getInt("image"));
     }
 }
--- app/src/main/java/com/cses/forage/OrderActivity.java
+++ app/src/main/java/com/cses/forage/OrderActivity.java
@@ -1,10 +1,12 @@
 package com.cses.forage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,7 +16,11 @@ import com.dexafree.materialList.card.CardProvider;
 import com.dexafree.materialList.listeners.RecyclerItemClickListener;
 import com.dexafree.materialList.view.MaterialListView;
 
+import java.util.ArrayList;
+
 public class OrderActivity extends AppCompatActivity {
+
+    private int[] itemRes = {R.drawable.clam_chowder, R.drawable.apple_cider, R.drawable.cake, R.drawable.mocha};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +47,7 @@ public class OrderActivity extends AppCompatActivity {
                     .setLayout(R.layout.material_list_custom_item)
                     .setTitle(itemName)
                     .setDescription(String.format("$%.2f", price))
-                    .setDrawable(getDrawable(R.drawable.cake))
+                    .setDrawable(getDrawable(itemRes[i]))
                     .endConfig()
                     .build();
 
@@ -51,12 +57,23 @@ public class OrderActivity extends AppCompatActivity {
         listView.addOnItemTouchListener(new RecyclerItemClickListener.OnItemClickListener() {
             @Override
             public void onItemClick(@NonNull Card card, int position) {
-                
+
+                Intent acceptOrder = new Intent(OrderActivity.this, ItemActivity.class);
+                CardProvider provider = card.getProvider();
+
+                Bundle extras = new Bundle();
+                String description = provider.getDescription();
+                extras.putInt("image", itemRes[position]);
+                extras.putString("itemName", provider.getTitle());
+                extras.putDouble("cost", Double.parseDouble(description.substring(1, description.length())));
+                acceptOrder.putExtras(extras);
+
+                startActivity(acceptOrder);
+
             }
 
             @Override
             public void onItemLongClick(@NonNull Card card, int position) {
-
             }
         });
 
