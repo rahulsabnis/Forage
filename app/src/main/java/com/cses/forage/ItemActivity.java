e12209038fa983c871a4e06a8b01b375fc6dc332
Rahul Sabnis <rahulsabnis19@gmail.com>
2017-07-30 10:28:04 -0700

    Add uber button to alertdialog and removed border from app logo

--- app/src/main/java/com/cses/forage/ItemActivity.java
+++ app/src/main/java/com/cses/forage/ItemActivity.java
@@ -1,11 +1,14 @@
 package com.cses.forage;
 
+import android.*;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatButton;
@@ -13,11 +16,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.travijuu.numberpicker.library.Enums.ActionEnum;
 import com.travijuu.numberpicker.library.Interface.ValueChangedListener;
 import com.travijuu.numberpicker.library.NumberPicker;
+import com.usebutton.sdk.ButtonContext;
+import com.usebutton.sdk.ButtonDropin;
+import com.usebutton.sdk.context.Location;
 
 public class ItemActivity extends AppCompatActivity {
+
+    private ButtonDropin mDropin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +43,15 @@ public class ItemActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog dialog = new AlertDialog.Builder(ItemActivity.this)
-                        .setTitle("Success!")
-                        .setMessage("Your order was successfully placed!")
-                        .setPositiveButton("OK", null)
-                        .create();
+                MaterialDialog dialog = new MaterialDialog.Builder(ItemActivity.this)
+                        .title("Success!")
+                        .customView(R.layout.dialog, true)
+                        .positiveText("OK")
+                        .build();
+
+                mDropin = (ButtonDropin) dialog.getCustomView().findViewById(R.id.deliveryButton);
+                setupButton();
+                dialog.show();
                 dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
                     @Override
                     public void onDismiss(DialogInterface dialog) {
@@ -46,7 +59,6 @@ public class ItemActivity extends AppCompatActivity {
                         startActivity(acceptOrder);
                     }
                 });
-                dialog.show();
             }
         });
 
@@ -62,7 +74,29 @@ public class ItemActivity extends AppCompatActivity {
                 view.setText(String.format("Total Cost: $%.2f", updatedCost));
             }
         });
+
         AppBarLayout layout = (AppBarLayout) findViewById(R.id.app_bar);
         layout.setBackgroundResource(extras.getInt("image"));
+    }
+
+    private void setupButton() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        // Setup your context
+        final Location location = new Location("Here", 37.563724, -122.325342);
+        final ButtonContext context = ButtonContext.withSubjectLocation(location);
+
+        // Prepare the Button for display with our context
+        mDropin.prepareForDisplay(context);
     }
 }
--- app/src/main/java/com/cses/forage/MainActivity.java
+++ app/src/main/java/com/cses/forage/MainActivity.java
@@ -1,12 +1,9 @@
 package com.cses.forage;
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 
 import com.dexafree.materialList.card.Card;
 import com.dexafree.materialList.card.CardProvider;
@@ -17,17 +14,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.usebutton.sdk.ButtonContext;
 import com.usebutton.sdk.ButtonDropin;
-import com.usebutton.sdk.context.Location;
-import com.usebutton.sdk.util.LocationProvider;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ButtonDropin mDropin;
+
     private ArrayList<Vendor> vendorArrayList = new ArrayList<Vendor>();
 
     private MaterialListView mListView;
@@ -41,8 +34,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         com.usebutton.sdk.Button.getButton(this).start();
-
-        mDropin = (ButtonDropin) findViewById(R.id.deliveryButton);
 
         mListView = (MaterialListView) findViewById(R.id.material_listview);
 
@@ -67,10 +58,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                     createCard(name, location, menu, imageRes[i]);
                     i++;
-                }
-
-                if (i == 0) {
-                    showButton();
                 }
             }
 
@@ -112,34 +99,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    private void showButton() {
-
-        final ButtonContext context = new ButtonContext();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        final android.location.Location loc = new LocationProvider(this).getBestLocation();
-
-//        if (loc != null) {
-//            final Location location = new Location(loc);
-//            context.setUserLocation(location);
-//        } else {
-
-            final Location location = new Location("Here", 37.563724, -122.325342);
-            context.setUserLocation(location);
-//        }
-
-        mDropin.prepareForDisplay(context);
     }
 
     private void createCard(String title, String description, ArrayList<MenuItem> menu,
